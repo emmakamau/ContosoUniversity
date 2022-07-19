c46982e07b798c5c1d41e2d1b046bd143070c28e
Emmaculate Kamau <ekamau@637capital.com>
2022-07-19 21:00:09 +0300

    Add sorting functionality on index razor page

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -8,25 +8,71 @@ using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 
+
+//Add Sorting Functionality on Last Name and Enrollment date
+//https://docs.microsoft.com/en-us/aspnet/core/data/ef-rp/sort-filter-page?view=aspnetcore-6.0
 namespace ContosoUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly ContosoUniversity.Data.SchoolContext _context;
-
-        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        private readonly SchoolContext _context;
+        public IndexModel(SchoolContext context)
         {
             _context = context;
         }
 
-        public IList<Student> Student { get;set; } = default!;
+        public string? NameSort { get; set; }
+        public string? DateSort { get; set; }
+        public string? CurrentFilter { get; set; }
+        public string? CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public IList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            if (_context.Students != null)
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+
+            switch (sortOrder)
             {
-                Student = await _context.Students.ToListAsync();
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
             }
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
+    //public class IndexModel : PageModel
+    //{
+    //    private readonly ContosoUniversity.Data.SchoolContext _context;
+
+    //    public IndexModel(ContosoUniversity.Data.SchoolContext context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    public IList<Student> Student { get;set; } = default!;
+
+    //    public async Task OnGetAsync()
+    //    {
+    //        if (_context.Students != null)
+    //        {
+    //            Student = await _context.Students.ToListAsync();
+    //        }
+    //    }
+    //}
 }
