3d48843632df8e622b9adc8072cc2ff26ebaff90
Emmaculate Kamau <ekamau@637capital.com>
2022-07-19 21:24:13 +0300

    Add an About page with data groups

---
+++ ContosoUniversity/Pages/About.cshtml.cs
@@ -0,0 +1,42 @@
+using ContosoUniversity.Models.SchoolViewModels;
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public AboutModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<EnrollmentDateGroup> Students { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            /*
+            The LINQ statement groups the student entities by enrollment date, calculates 
+            the number of entities in each group, and stores the results in a collection of 
+            EnrollmentDateGroup view model objects.
+             */
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+
+            Students = await data.AsNoTracking().ToListAsync();
+        }
+    }
+}
